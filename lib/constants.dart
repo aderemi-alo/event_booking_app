1cae212edcca18a23a5b0649a039a0f0ade7ade8
FlutterBugs <aloaderemi1@gmail.com>
2023-05-09 09:47:42 +0100

    Added the typography from the design file

---
+++ lib/constants.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// Typography for the application
+final TextStyle heading1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700, fontSize: 50, height: 72);
+
+final TextStyle heading2 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w300, fontSize: 35);
+
+final TextStyle heading3 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w300, fontSize: 30);
+
+final TextStyle heading4 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w500, fontSize: 24);
+
+final TextStyle heading5 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500, fontSize: 22, height: 34);
+
+final TextStyle title1 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w500, fontSize: 18);
+
+final TextStyle title2 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w500, fontSize: 16);
+
+final TextStyle title3 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w500, fontSize: 15);
+
+final TextStyle body1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w300, fontSize: 16, height: 25);
+
+final TextStyle body2 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w300, fontSize: 15);
+
+final TextStyle body3 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w300, fontSize: 14, height: 23);
+
+final TextStyle button = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500, fontSize: 16, height: 25);
+
+final TextStyle subtitle1 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w300, fontSize: 13);
+
+final TextStyle subtitle2 =
+    GoogleFonts.montserrat(fontWeight: FontWeight.w300, fontSize: 12);
